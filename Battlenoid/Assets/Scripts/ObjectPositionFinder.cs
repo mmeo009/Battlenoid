44ac4da3f7252c4b5d19e832db367712603679d6
mmeo009 <shh020615@gmail.com>
2024-02-08 22:36:32 +0900

    사소한 오류 수정

--- Battlenoid/Assets/Scripts/ObjectPositionFinder.cs
+++ Battlenoid/Assets/Scripts/ObjectPositionFinder.cs
@@ -17,7 +17,15 @@ public class ObjectPositionFinder : MonoBehaviour
         {
             ObjectData.me = this.gameObject;
         }
-        ObjectManager.Instance.objectDictionary.Add($"{ObjectData.x},{ObjectData.y},{ObjectData.z}", ObjectData);
+        if(ObjectManager.Instance.objectDictionary.ContainsKey($"{ObjectData.x},{ObjectData.y},{ObjectData.z}") == false)
+        {
+            ObjectManager.Instance.objectDictionary.Add($"{ObjectData.x},{ObjectData.y},{ObjectData.z}", ObjectData);
+        }
+
+        if (this.ObjectData.myType == ObjectData.Type.CUBEGRID && ObjectManager.Instance.cubeArray[this.ObjectData.x, this.ObjectData.z] == null)
+        {
+            ObjectManager.Instance.cubeArray[this.ObjectData.x, this.ObjectData.z] = new CubeGrid(this.ObjectData.x, 0, this.ObjectData.z, false);
+        }
         Destroy(this);
     }
 }
